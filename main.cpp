b5fc4b1e3d47b29a4c1d71c5a4003436a446b0a2
Wolf <wolf@wolfsden.cz>
2016-06-08 15:57:32 +0200

    implemented abs for positive numbers

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+#include <cstdint>
+#include <cstddef>
+
+/*
+ * Get absolute value of n, supports only positive numbers.
+ *
+ * So don't pass anything negative in here.
+ */
+int64_t abs(int64_t n) {
+	if (n > 0) {
+		return n;
+	} else {
+		++*(int*)NULL;
+	}
+}
 
 int main(int, char**) {
 	return 0;
