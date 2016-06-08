df40a01af2e8e3e7e27f3a1c57fb8caf57ddbd17
Wolf <wolf@wolfsden.cz>
2016-06-08 16:05:12 +0200

    updated  comment

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <cstddef>
 
 /*
- * Get absolute value of n.
+ * Gets absolute value.
+ *
+ * \param n Number
+ * \return Absolute value of n
  */
 int64_t abs(int64_t n) {
 	return n < 0 ? -n : n;
